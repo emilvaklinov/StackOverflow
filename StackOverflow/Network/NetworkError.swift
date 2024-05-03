437029590a2119943d9689e0107cc962ce773869
Emil Vaklinov <emil_vaklinov@yahoo.com>
2024-05-03 17:36:11 +0100

    Network layer created

---
+++ StackOverflow/Network/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkError.swift
+//  StackOverflow
+//
+//  Created by Emil Vaklinov on 03/05/2024.
+//
+
+import Foundation
---
+++ StackOverflow/Network/StackOverflowAPI.swift
@@ -0,0 +1,8 @@
+//
+//  StackOverflowAPI.swift
+//  StackOverflow
+//
+//  Created by Emil Vaklinov on 03/05/2024.
+//
+
+import Foundation
